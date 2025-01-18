a979d81a59bfbc8569e753345d9935f5272a800b
Rahmonqulzoda Abdulhalim <crypto9by@gmail.com>
2025-01-18 14:04:36 +0300

    uy

--- src/pages/caseStudies/caseStudies.tsx
+++ src/pages/caseStudies/caseStudies.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import img from "./picturesAb/call.png";
+import img1 from "./picturesAb/buttons.png";
+import img2 from "./picturesAb/illustration.png";
+import img3 from "./picturesAb/illustration (1).png";
 import "./case.css";
 const CaseStudies = () => {
   return (
-    <div className="pt-[30px]">
+    <div className="max-w-[1200px] mx-auto">
+    <div className="">
       <div className=" fail h-[75vh]">
         <p className="text-center text-[white] text-[42px] pt-[120px]">
           Our services
@@ -23,10 +27,10 @@ const CaseStudies = () => {
           </div>
         </div>
       </div>
-      <div className="">
-        <div className="">
-          <p className="">Social Media Marketing</p>
-          <p className="">
+      <div className="flex justify-center items-center mt-[120px] gap-[40px]">
+        <div className="flex flex-col gap-[15px]">
+          <p className="text-[35px]">Social Media Marketing</p>
+          <p className="w-[500px]">
             Eleifend orci massa et facilisis orci feugiat ac. Congue in ut
             lacus, turpis accumsan gravida. Aliquet mattis dignissim massa
             sociis a, id bibendum. Egestas lorem eros, mi cras amet, vel eget
@@ -35,11 +39,32 @@ const CaseStudies = () => {
             pretium.
           </p>
           <div className="">
-            <button></button>
-            <button></button>
+            <img src={img1} alt="" />
+          </div>
+        </div>
+        <div className="">
+          <img src={img2} className="w-[500px]" alt="" />
+        </div>
+      </div>
+      <div className="">
+        <div className="">
+          <img src={img3} className="w-[670px]" alt="" />
+        </div>
+        <div className="flex flex-col gap-[15px]">
+          <p className="text-[35px]">Social Media Marketing</p>
+          <p className="w-[500px]">
+            Elementum ipsum lorem tortor morbi quis turpis pellentesque ultrices
+            aenean. Lacus sit viverra et egestas nisl vitae lacus, eget. Eu
+            pulvinar duis in posuere non adipiscing. Nunc, tristique viverra
+            tincidunt nec proin tempus egestas massa. Quis velit nunc nisl sem
+            in tristique.
+          </p>
+          <div className="">
+            <img src={img1} alt="" />
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
